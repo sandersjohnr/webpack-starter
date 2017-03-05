480b8eddf2a81f08f9de1238136a5437f763ce2d
John Sanders <john.sanders@cittacorp.com>
2017-03-05 16:29:51 -0500

    Installed html-webpack-plugin and extract-text-plugin

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: 'dist',
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader'],
+          publicPath: '/dist'
+        })
+      }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Project Demo',
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: './src/index.ejs'
+    }),
+    new ExtractTextPlugin({
+      filename: 'app.css',
+      allChunks: true
+    })
+  ]
+}
